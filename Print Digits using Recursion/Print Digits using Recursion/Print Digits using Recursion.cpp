27c3a3615377b477bbe5c28783fc606c09bb486c
samira-ahmed-20 <samirasakr2005@gmail.com>
2026-02-28 23:40:43 +0200

    Add files via upload

---
+++ Print Digits using Recursion/Print Digits using Recursion/Print Digits using Recursion.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+void print(int n){
+    if (n <= 0)
+        return;
+    
+    print(n / 10);
+    cout << n % 10 << endl;
+}
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--) {
+        int num;
+        cin >> num;
+        if (num == 0) {
+            cout << "0";
+        }
+        else 
+            print(num);
+
+        cout << endl;
+    }
+    return 0;
+}
