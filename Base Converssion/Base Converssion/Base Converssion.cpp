69223eb4057d252077704fd3e7162e1e30370254
samira-ahmed-20 <samirasakr2005@gmail.com>
2026-02-28 23:35:03 +0200

    Add files via upload

---
+++ Base Converssion/Base Converssion/Base Converssion.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+ 
+void printBinary(int n) {
+    if (n == 0)
+        return;  
+    printBinary(n / 2);      
+    cout << n % 2;
+}
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        int num;
+        cin >> num;
+        if (num == 0)
+            cout << 0;  
+        else
+            printBinary(num);
+        cout << endl;
+    }
+    return 0;
+}
