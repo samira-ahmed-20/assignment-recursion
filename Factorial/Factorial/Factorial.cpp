d5e390d6b09a714eb17ba23a9eae7e5f5e3df206
samira-ahmed-20 <samirasakr2005@gmail.com>
2026-02-28 23:36:53 +0200

    Add files via upload

---
+++ Factorial/Factorial/Factorial.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+int factorial(int n) {
+    if (n == 0 || n == 1)  
+        return 1;
+    return n * factorial(n - 1); 
+}
+int main() {
+    int N;
+    cin >> N;
+    cout << factorial(N) << endl;
+
+    return 0;
+}
